bd4e2ee8ff266f9ea723816597fddde303816e98
Abdelwahed <abdelwahedsouid@gmail.com>
2024-10-02 17:27:27 +0100

    test email2

--- src/main/java/com/projet/tpachatproject/controllers/ApiController.java
+++ src/main/java/com/projet/tpachatproject/controllers/ApiController.java
@@ -1,4 +1,10 @@
 package com.projet.tpachatproject.controllers;
 
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class ApiController {
 }
