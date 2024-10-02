fd503a248e854ae35f9eca3fa1cc31808632c9fd
Abdelwahed <abdelwahedsouid@gmail.com>
2024-10-02 17:22:55 +0100

    test email

--- src/main/java/com/projet/tpachatproject/controllers/FactureRestController.java
+++ src/main/java/com/projet/tpachatproject/controllers/FactureRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/facture")
-@Tag(name = "Gestion des Facturessssssss")
+@Tag(name = "Gestion des Factures" )
 @CrossOrigin("*")
 @AllArgsConstructor
 public class FactureRestController {
