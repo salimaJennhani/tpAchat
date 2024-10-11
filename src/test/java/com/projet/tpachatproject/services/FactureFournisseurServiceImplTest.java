2643d4497f6f67b4f67b385eb4bd0e729eaa08b3
dridikalthoum <kalthoum.dridi@esprit.tn>
2024-10-11 16:03:59 +0100

    correctedtestname

--- src/test/java/com/projet/tpachatproject/services/FactureFournisseurServiceImplTest.java
+++ src/test/java/com/projet/tpachatproject/services/FactureFournisseurServiceImplTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.*;
 
 @Slf4j
 @ExtendWith(MockitoExtension.class)
-public class FactureServiceImplTest {
+public class FactureFournisseurServiceImplTest {
 
 
     @Mock
